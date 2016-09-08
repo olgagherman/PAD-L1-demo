b869cfcd5899f11ef4fdb0ecd6a7a28afc0bedab
Alexandr G <alexandr@gavrisco.com>
2016-09-08 10:27:09 +0300

    Add logging config and entry point

---
+++ src/manage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import logging.config
+from notibroker.broker import run_server
+
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s | %(asctime)s | %(name)s:%(funcName)s | %(process)d | %(thread)d | %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'notibroker': {
+            'level': 'DEBUG',
+            'handlers': ['console']
+        }
+    }
+})
+
+run_server()
