b01d3f943b7cc8cef5e2b3bc1a002edc943a3980
Gherman Olga <gherman.olya@gmail.com>
2016-10-18 09:52:39 +0300

    adds a function witch get an id for receiver
    and receiver specifies to delete its queue after emptying or not

--- src/receiver.py
+++ src/receiver.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import asyncio
 import json
+from pathlib import Path
+import random
 
-async def get_message(loop):
+async def get_message(loop, name):
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 14141, loop=loop
     )
+    bools = [True, False]
+    persistent = random.shuffle(bools)
     writer.write(json.dumps({
         'type': 'command',
-        'command': 'read'
+        'command': 'read',
+        'destination': name.rstrip(),
+        'persistent_queue': persistent
     }).encode('utf-8'))
     writer.write_eof()
     await writer.drain()
@@ -16,11 +22,29 @@ async def get_message(loop):
     writer.close()
     return response
 
+async def get_destination_name():
+    path = Path("free_users.txt")
+    if path.is_file():
+        with open("free_users.txt", 'r+') as f:
+            all_names = f.readlines()
+            length = len(all_names)
+            f.seek(0)
+            for i, val in enumerate(all_names):
+                if i == 0:
+                    name = val
+                else:
+                    f.write(val)
+            f.truncate()
+            f.close()
+        with open("connected_users.txt", "a") as f:
+            f.write(name)
+    return name
 
 async def run_receiver(loop):
+    name = await get_destination_name()
     while True:
         try:
-            response = await get_message(loop)
+            response = await get_message(loop, name)
             print('Received %s', response)
             await asyncio.sleep(1)
         except KeyboardInterrupt:
