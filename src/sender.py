0255eff1f2c8dbe08135a8b6e295f0b7a4daacc5
Gherman Olga <gherman.olya@gmail.com>
2016-10-18 09:54:13 +0300

    adds a function witch get the names of connected receivers
    sender chooses random a receiver and sends the message

--- src/sender.py
+++ src/sender.py
@@ -2,14 +2,17 @@
 import asyncio
 import json
 import uuid
+from pathlib import Path
+import random
 
-async def send_message(message, loop):
+async def send_message(message, destination, loop):
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 14141, loop=loop
     )
     payload = json.dumps({
         'type': 'command',
         'command': 'send',
+        'destination': destination,
         'payload': message
     }).encode('utf-8')
 
@@ -21,13 +24,27 @@ async def send_message(message, loop):
     writer.close()
     return response
 
+async def get_receiver_name():
+    path = Path("connected_users.txt")
+    if path.is_file():
+        with open("connected_users.txt", 'r') as f:
+            names = f.readlines()
+            length = len(names)
+            if length == 0:
+                print("THERE'S NO MORE IDS")
+                name = '000000'
+            else:
+                index = random.randint(0, length - 1)
+                name = names[index]
+    return name.rstrip()
 
 async def run_sender(loop):
     while True:
+        destination = await get_receiver_name()
         try:
             message = 'Just sending a random %s' % (uuid.uuid4().hex,)
             print('Sending %s' % (message,))
-            response = await send_message(message, loop)
+            response = await send_message(message, destination, loop)
             print('Received %s', response)
             await asyncio.sleep(1)
         except KeyboardInterrupt:
